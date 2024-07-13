ac8c768ac5ae2acadb56cad1340f6d092275fadc
LennySoltan <123989660+Takutotfu@users.noreply.github.com>
2024-07-13 02:58:57 +0200

    Update run.bat

--- JavaFX_project/run.bat
+++ JavaFX_project/run.bat
@@ -1,7 +1,10 @@
 @echo off
 
-javac --module-path "%PATH_TO_FX%" --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web Main.java
+:: Set your JavaFX lib folder path
 
+set PATH_TO_FX = "C:\path\to\javafx\lib"
+
+javac --module-path "%PATH_TO_FX%" --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web Main.java
 java --module-path "%PATH_TO_FX%" --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.web Main
 
 erase *.class
